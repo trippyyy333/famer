fbf16900284c205b645c5eb6abf2fd9347c36da2
trippyyy333 <75495653+trippyyy333@users.noreply.github.com>
2023-05-27 14:11:09 -0400

    zaz
    fd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactdfhyk;iphfh
         </a>
       </header>
     </div>
