49c6bc43d8df52fdcf0732bf1f825398e98f7899
trippyyy333 <75495653+trippyyy333@users.noreply.github.com>
2023-05-27 14:22:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactdfhyk;iphfh
+          Learn Reactdftgerhykiphfhrwer
         </a>
       </header>
     </div>
